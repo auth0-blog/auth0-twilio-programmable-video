7a7d6a68497ae16e51933aba7dae874653fb46fa
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-29 18:29:52 -0300

    showing local video;

--- src/components/VideoConference.js
+++ src/components/VideoConference.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {connect, createLocalVideoTrack} from 'twilio-video';
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2Q0OGRjMzc2YzFkYTMzY2U0ODA4NTUxNWRhMWQ4MzA0LTE1NDA4NDU3NTUiLCJpc3MiOiJTS2Q0OGRjMzc2YzFkYTMzY2U0ODA4NTUxNWRhMWQ4MzA0Iiwic3ViIjoiQUM3YTEzOGM1ZjNiODRhZGI3YzFhYzg1YTE5YTk3NGUwOCIsImV4cCI6MTU0MDg0OTM1NSwiZ3JhbnRzIjp7ImlkZW50aXR5IjoicmVhY3QtYXBwIiwidmlkZW8iOnt9fX0.XqdXcELEDjXX8B6txQnz5WuL39kvaVj-wNjub3FhSnQ';
+// TODO get access token from a secure backend
+const token = 'some-access-token';
 
 class VideoConference extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'Loading ...',
+      title: 'Loading ...',
     }
   }
 
@@ -16,19 +17,29 @@ class VideoConference extends Component {
     try {
       const room = await connect(token, {name: this.props.videoCallId});
       room.on('participantConnected', participant => {
-        console.log('joined', participant);
+        console.log(`Participant "${participant.identity}" connected`);
+
+        participant.tracks.forEach(publication => {
+          if (publication.isSubscribed) {
+            const track = publication.track;
+            document.getElementById('remote-media-div').appendChild(track.attach());
+          }
+        });
+
+        participant.on('trackSubscribed', track => {
+          document.getElementById('remote-media-div').appendChild(track.attach());
+        });
       });
 
       const track = await createLocalVideoTrack();
       const localMediaContainer = document.getElementById('local-video');
       localMediaContainer.appendChild(track.attach());
       this.setState({
-        message: `Joined Room: ${room.name}`,
+        title: `Joined Room: ${room.name}`,
       });
     } catch (error) {
-      console.log('error.message', error.message);
       this.setState({
-        message: error.message,
+        title: error.message,
       });
     }
   }
@@ -37,10 +48,13 @@ class VideoConference extends Component {
     return (
       <div className="row">
         <div className="col-12">
-          <h1 className="text-white">{this.state.message}</h1>
+          <h1 className="text-white">{this.state.title}</h1>
         </div>
         <div className="col-6">
           <div id="local-video" />
+        </div>
+        <div className="col-6">
+          <div id="remote-media-div" />
         </div>
       </div>
     );
